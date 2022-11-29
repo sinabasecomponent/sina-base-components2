a3b744d9a0e9b36336ecaaea46294a6d0b663faf
ali mahmoodi <alimahmoudi.alami@gmail.com>
2022-11-29 21:52:04 +0330

    fix rollup

--- src/components/rollup.config.mjs
+++ src/components/rollup.config.mjs
@@ -1,11 +1,11 @@
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import packageJson from "./package.json" assert { type: "json" };
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import packageJson from "./package.json" assert { type: "json" };
 
 export default [
   {
@@ -33,9 +33,7 @@ export default [
   },
   {
     input: "dist/esm/types/index.d.ts",
-    output: [
-      { file: "dist/index.d.ts", format: "esm" },
-    ],
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
     external: [/\.css$/],
   },
